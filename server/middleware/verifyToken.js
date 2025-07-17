6a7b040ea957527d37410243964b1d5062002127
Sanjay <sanjay4subbu@gmail.com>
2025-07-17 23:03:51 +0530

    added middleware

---
+++ server/middleware/verifyToken.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+
+const verifytoken=(req,res,next)=>{
+    const authheader = req.headers.authorization;
+
+
+    if (!authheader || !authheader.startsWith("Bearer ")){
+        return res.status(401).json({ error: "No token provided"})
+    }
+
+    const token = authheader.split(" ")[1]
+
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded
+        next();
+    } catch(err) {
+        return res.status(403).json({error:"Invalid token"})
+    }
+};
+
+export default verifytoken;
