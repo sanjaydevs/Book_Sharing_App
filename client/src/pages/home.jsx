e4917a5569cb2e1f12338c6358b80246b86dd5f5
Sanjay <sanjay4subbu@gmail.com>
2025-07-19 11:14:25 +0530

    Tailwind@3 installed

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,7 +1,7 @@
 export default function Home(){
     return (
         <div className="flex flex-col items-center justify-center h-screen">
-            <h1 className="text-4xl font-bold text-blue-600">Welcome to BookShare 📚</h1>
+            <h1 className="text-4xl font-bold text-blue-600">Welcome to NITC BookShare 📚</h1>
             <p className="mt-4 text-lg text-gray-700">Find, borrow, and share books with your community.</p>
         </div>
     );
